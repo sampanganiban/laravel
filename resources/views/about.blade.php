aaedd7d92c527b59ba93f9de8a23084dc3edc854
Samantha Panganiban <samantha.isabellechivon@gmail.com>
2015-09-14 13:04:48 +1200

    Add a route for a page and a view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Welcome Page</title>
+	<meta charset="UTF-8">
+	<title>About Us</title>
 </head>
 <body>
-    
+	
 </body>
 </html>
